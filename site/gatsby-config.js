6c02f546c0cc6e3565102a0fb0c4aa0e0886e06e
Jefferson Bruno Venero da Silva <jbvsilva@paschoalotto.com.br>
2022-11-03 21:05:45 -0300

    adiconamdo as redes sociais informaçoes do site

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -17,26 +17,27 @@ module.exports = {
   // Customize your site metadata:
   siteMetadata: {
     //General Site Metadata
-    title: 'FlexiBlog Theme',
-    name: 'FlexiBlog',
-    description: 'My site description...',
-    address: 'New York, NY',
-    email: 'email@example.com',
-    phone: '+1 (888) 888-8888',
+    title: 'G5 NEWS',
+    name: 'G5News',
+    description: 'Goiás em tempo real',
+    address:
+      'Av. T-4, Ed. Buena Vista Office Design - 16° andar - 1613 Goiânia - GO',
+    email: 'contato@g5news.com.b',
+    phone: '(62) 3988-7592',
 
     //Site Social Media Links
     social: [
       {
-        name: 'Github',
-        url: 'https://github.com/gatsbyjs'
+        name: 'Facebook',
+        url: 'https://www.facebook.com/siteg5news'
       },
       {
         name: 'Twitter',
-        url: 'https://twitter.com/gatsbyjs'
+        url: 'https://twitter.com/SiteG5News'
       },
       {
         name: 'Instagram',
-        url: 'https://github.com/gatsbyjs'
+        url: 'https://www.instagram.com/g5newsgoias/'
       }
     ],
 
