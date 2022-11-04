2f9329c78ce88cb1c3338437c280693604ac7a47
Jefferson Bruno Venero da Silva <jbvsilva@paschoalotto.com.br>
2022-11-03 21:40:13 -0300

    Pagina de quem somos

---
+++ packages/blog/gatsby-blog-pages/src/pages/about.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Layout, Stack, Main, Sidebar } from '@layout'
+import PageTitle from '@components/PageTitle'
+import Divider from '@components/Divider'
+import Seo from '@widgets/Seo'
+import ContactForm from '@widgets/ContactForm'
+import ContactInfo from '@widgets/ContactInfo'
+import Commitment from '@widgets/Commitment'
+
+const PageAbout = props => (
+  <Layout {...props}>
+    <Seo title='Quem Somos' />
+    <Divider />
+    <Stack>
+      <Main>
+        <PageTitle
+          header="Quem Somos"
+          subheader='O Site G5News é voltado para todas as classes, com forte atuação em Goiás. Conta hoje com média de 50 mil visitas diárias e segue uma linha editorial moderada, mas de forte opinião. Nossa equipe é composta por jornalistas gabaritados e comprometidos com a ética e a verdade dos fatos.'
+        />
+        <Divider />
+        <ContactForm />
+      </Main>
+      <Sidebar>
+        <Commitment />
+        <Divider />
+        <ContactInfo />
+      </Sidebar>
+    </Stack>
+  </Layout>
+)
+
+export default PageAbout
